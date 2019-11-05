dd791e5191b6dd527961f78bfc7dedd7e049e9ff
Festive <27864684+mash-reza@users.noreply.github.com>
2019-11-05 11:26:46 +0330

    menu activity created

---
+++ app/src/main/java/com/example/subwayfinder/view/Menu.java
@@ -0,0 +1,20 @@
+package com.example.subwayfinder.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.example.subwayfinder.R;
+
+import butterknife.BindView;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
